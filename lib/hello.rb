cd3a9da1253c18b2d2f86fa55da5649450dafda5
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 22:12:16 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -1,6 +1,9 @@
 def hello_t(names)
-  names.each do {|name| puts name}
+  ["Tim", "Tom", "Jim"].each do |name|
+    if name.start_with?("T")
+      puts "Hi, #{name}"
+    end
+  end
 end
 
 # call your method here!
-
