06d97f233db53789d07d37ac440a1276a8356155
Alicia Yu <alicia.yu.16@gmail.com>
2017-10-04 22:13:20 +0000

    automatically backed up by learn

--- lib/hello.rb
+++ lib/hello.rb
@@ -2,7 +2,7 @@ def hello_t(names)
   ["Tim", "Tom", "Jim"].each do |name|
     if name.start_with?("T")
       puts "Hi, #{name}"
-    else 
+    else
       puts "Hey! No block was given!"
     end
   end
